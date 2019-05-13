c95c2f21f2b4f5cf30ee1b1d05db86bcfe3f798c
Josh Chan <chan.joshua.j@gmail.com>
2019-05-13 14:59:37 -0400

    chal4

---
+++ GlobalObj.js
@@ -0,0 +1,24 @@
+    var navigation = {
+      x: -2,
+      y: 4,
+      z: 7,
+      speed: "raaaaid"
+    };
+
+    var ship = {
+      powerOn: false,
+      modules: [],
+      antenna: {
+        active: false
+      }
+    };
+
+    var radio = {
+      range: {
+        low: 88,
+        high: 108,
+      },
+      frequency: 0,
+      message: "Bugs are cool.",
+      beacon: false
+    };
--- chal4.js
+++ chal4.js
@@ -1,17 +1,18 @@
-function countModules(){
-	return availableModules.length
-}
-function countEssential(){
-	var count = 0;
-	for(i = 0;i < availableModules.length;i++){
-		if (availableModules[i].essential === true){
-			count++;
-		}
-	}
-	return count;
-}
+// Problems Encountered:
+// 1. not able to input correct index number since "Cannot read property 'essential' of undefined", must be a problem in loopModule
 function loadModule(index) { 
-	if(availableModules[i].essential !== true){
-		availableModules[i].essential = true;
-	}
+  if(availableModules[index].essential !== true){ //if the essential flag from the module is true
+    return null;
+  }
+    availableModules[index].enabled = true;       //change enabled flag to be true
+    ship.modules.push(availableModules[index]);   //add object to ship.modules array
+  
 }
+function loopModule(){  
+ for(i = 0;i < availableModules.length;i++){  //loop through array of Module objects
+   if (availableModules[i].name === "life-support"){ //find Module with name property "life-support"
+     loadModule(i);   //load that module using its index in array 
+   }
+ }
+}
+loopModule(); //call loopModule
