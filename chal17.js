02aae40d10f82de791b7e5df1f86bc5aabc510af
Josh Chan <chan.joshua.j@gmail.com>
2019-05-17 19:59:57 -0400

    chal17

---
+++ chal17.js
@@ -0,0 +1,6 @@
+/*Write a function called activateAntenna() which will set the active property on the antenna to true.*/
+
+function activateAntenna(){
+  ship.antenna.active = true;
+}
+activateAntenna();
