f66aea6f41b135b18dd8f243a25ee8a10c0368ac
Josh Chan <chan.joshua.j@gmail.com>
2019-05-18 19:56:45 -0400

    chal18

---
+++ chal18.js
@@ -0,0 +1,9 @@
+//Create a new function called sendBroadcast(). In this function you'll need to write a loop to call the newly-enabled broadcast() function 100 times 
+
+
+function sendBroadcast(){
+  for(i = 0;i<100;i++){
+    broadcast();
+  }
+}
+sendBroadcast();
